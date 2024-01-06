56787bcec4fff7bb6edbea445b71df072baeb5ab
Rupesh Ghimire <rupeshghimire007@gmail.com>
2024-01-06 18:18:05 +0545

    Raw SQL used for database creation

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = '10be645508f6044194986e98faf269b8'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ database.py
@@ -0,0 +1,55 @@
+import sqlite3
+
+# open database
+conn = sqlite3.connect('database.db')
+
+# create table
+conn.execute('''CREATE TABLE users
+            (
+            userID INTEGER PRIMARY KEY,
+            password TEXT,
+            email TEXT,
+            firstName TEXT,
+            lastName TEXT,
+            city TEXT,
+            state TEXT,
+            country TEXT,
+            phone TEXT
+            )''')
+
+
+# Context Table
+# Create Context
+conn.execute('''CREATE TABLE contexts
+            (
+                contextID INTEGER PRIMARY KEY,
+                summary TEXT,
+                userID INTEGER,
+                FOREIGN KEY (userID) REFERENCES users(userID)
+            )''')
+
+
+
+# Question Table
+# References Context
+conn.execute('''CREATE TABLE questions
+            (
+                questionID INTEGER PRIMARY KEY,
+                text TEXT,
+                contextID INTEGER,
+                FOREIGN KEY (contextID) REFERENCES contexts(contextID)
+            )''')
+
+
+# Answer Table
+# References Question
+conn.execute('''CREATE TABLE answers 
+            (
+                answerID INTEGER PRIMARY KEY,
+                text TEXT,
+                questionID INTEGER,
+                FOREIGN KEY (questionID) REFERENCES questions(questionID)
+            )''')
+
+
+conn.close()
