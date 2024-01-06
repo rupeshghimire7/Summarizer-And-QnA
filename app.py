8a6571af558785cfc295e5939a362fae53eb2bba
Rupesh Ghimire <rupeshghimire007@gmail.com>
2024-01-06 22:36:44 +0545

    Added templates and login function

--- app.py
+++ app.py
@@ -33,9 +33,38 @@ def getLoginDetails():
 
 
 
+@app.route("/login", methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        if is_valid(email, password):
+            session['email'] = email
+            return redirect(url_for('/'))
+        else:
+            error = 'Invalid UserID / Password'
+            return render_template('login.html', error=error)
+
+
+def is_valid(email, password):
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    cur.execute('SELECT email, password FROM Users')
+    data = cur.fetchall()
+    for row in data:
+        if row[0] == email and row[1] == hashlib.md5(password.encode()).hexdigest():
+            return True
+    return False
+
+
 @app.route('/')
 def home():
+    loggedIn, userID, firstName = getLoginDetails()
+
+    return render_template('home.html', loggedIn=loggedIn, userID=userID, firstName=firstName)
+
     
+
 
 
 
