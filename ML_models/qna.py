debaca6ed6af8e2330e80d968fd79b283d8bf21f
Rupesh Ghimire <rupeshghimire007@gmail.com>
2024-01-08 18:16:55 +0545

    QnA model changed to roberta-base-squad2

--- ML_models/qna.py
+++ ML_models/qna.py
@@ -1,13 +1,5 @@
 # To load and create a inference function for QnA when given a context
 from transformers import pipeline
 
-model_checkpoint = "consciousAI/question-answering-roberta-base-s-v2"
-question_answerer = pipeline("question-answering", model=model_checkpoint)
+inference_model_qna = pipeline("question-answering", model="deepset/roberta-base-squad2")
 
-
-def inference_model_qna(question, context):
-    answer = question_answerer(question=question, context=context)
-    return answer
-
-
-    
